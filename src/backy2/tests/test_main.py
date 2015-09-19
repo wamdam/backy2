a1312e2ef2b654b35dd74f761ab96b7bc343c933
Daniel Kraft <daniel.kraft@d9t.de>
2015-09-19 13:14:01 +0200

    Fixed tests

--- src/backy2/tests/test_main.py
+++ src/backy2/tests/test_main.py
@@ -2,7 +2,9 @@ import pytest
 import os
 import sys
 import backy2.main
+import shutil
 
+CHUNK_SIZE = 1024*4096
 
 @pytest.yield_fixture
 def argv():
@@ -25,17 +27,20 @@ usage: py.test [-h] [-v] [-b BACKUPDIR] {backup,restore,scrub} ...
     assert err == ""
 
 
+
+# Test Level
+
 def test_level_consistency():
     CHUNK_SIZE = 1024*4096
     s1 = os.urandom(CHUNK_SIZE)
     s2 = os.urandom(CHUNK_SIZE)
     s3 = os.urandom(CHUNK_SIZE)
-    with backy2.main.Level('_test.data', '_test.index') as lw:
+    with backy2.main.Level('_test.data', '_test.index', CHUNK_SIZE) as lw:
         lw.write(10, s1)
         lw.write(8, s2)
         lw.write(12, s3)
 
-    with backy2.main.Level('_test.data', '_test.index', remove=True) as lw:
+    with backy2.main.Level('_test.data', '_test.index', CHUNK_SIZE, temporary=True) as lw:
         assert lw.read(8) == s2
         assert lw.read(10) == s1
         assert lw.read(12) == s3
@@ -45,7 +50,7 @@ def test_level_wrong_size():
     s1 = os.urandom(10)
     s2 = os.urandom(10)
     s3 = os.urandom(11)
-    with backy2.main.Level('_test.data', '_test.index', remove=True) as lw:
+    with backy2.main.Level('_test.data', '_test.index', CHUNK_SIZE, temporary=True) as lw:
         lw.write(1, s1)
         lw.write(2, s2)
         with pytest.raises(backy2.main.BackyException):
@@ -56,25 +61,41 @@ def test_level_wrong_size_last_chunk():
     s1 = os.urandom(10)
     s2 = os.urandom(10)
     s3 = os.urandom(11)
-    with backy2.main.Level('_test.data', '_test.index') as lw:
+    with backy2.main.Level('_test.data', '_test.index', CHUNK_SIZE) as lw:
         lw.write(1, s1)
         lw.write(2, s2)
         lw.write(2, s3)
 
-    with backy2.main.Level('_test.data', '_test.index', remove=True) as lw:
+    with backy2.main.Level('_test.data', '_test.index', CHUNK_SIZE, temporary=True) as lw:
         assert lw.read(1) == s1
         assert lw.read(2) == s3
 
 
 def test_level_wrong_checksum(caplog):
     s1 = os.urandom(10)
-    with backy2.main.Level('_test.data', '_test.index') as lw:
+    with backy2.main.Level('_test.data', '_test.index', CHUNK_SIZE) as lw:
         lw.write(1, s1)
         # nasty hack, destroy checksum for chunk_id 1
-        lw.index[1]['checksum'] = 'haha'
+        lw.index[1].checksum = 'haha'
 
-    with backy2.main.Level('_test.data', '_test.index', remove=True) as lw:
+    with backy2.main.Level('_test.data', '_test.index', CHUNK_SIZE, temporary=True) as lw:
         lw.read(1)
         assert 'CRITICAL Checksum for chunk 1 does not match' in caplog.text()
 
+
+
+# Test Backup
+
+def test_backup():
+    path = '_testbackup'
+    src = 'src'
+    dst = 'dst'
+
+    os.mkdir(path)
+    with open(os.path.join(path, src), 'wb') as f:
+        f.write(os.urandom(4*CHUNK_SIZE))
+    backy = backy2.main.Backy(path, dst, CHUNK_SIZE)
+    backy.backup(os.path.join(path, src), dst)
+
+    shutil.rmtree(path)
 
