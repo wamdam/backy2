c32d1f10905f5b4621b98734b13f72d568529141
Daniel Kraft <daniel.kraft@d9t.de>
2015-10-25 12:08:19 +0100

    having something working...

--- src/backy2/backy.py
+++ src/backy2/backy.py
@@ -2,14 +2,17 @@
 
 #from prettytable import PrettyTable
 import argparse
+import configparser
 #import glob
-#import datetime
+import datetime
 import fileinput
 #import math
 #import hashlib
 import logging
 import json
 #import random
+import sqlite3
+import uuid
 import os
 import sys
 
@@ -46,6 +49,219 @@ def hints_from_rbd_diff(rbd_diff):
     return [(l['offset'], l['length'], True if l['exists']=='true' else False) for l in data]
 
 
+def makedirs(path):
+    try:
+        os.makedirs(path)
+    except FileExistsError:
+        pass
+
+
+class MetaBackend():
+    """ Holds meta data """
+
+    def __init__(self, path):
+        self.path = path
+
+
+    def create_version(self, version_name, size):
+        """ Creates a new version with a given name.
+        size is the number of blocks this version will contain.
+        Returns a uid for this version.
+        """
+        raise NotImplementedError()
+
+
+    def get_version(self, uid):
+        """ Returns a version as a dict """
+        raise NotImplementedError()
+
+
+    def set_block(self, id, version_uid, block_uid, checksum, size):
+        """ Set a block to <id> for a version's uid (which must exist) and
+        store it's uid (which points to the data BLOB).
+        checksum is the block's checksum
+        size is the block's size
+        """
+        raise NotImplementedError()
+
+
+    def close(self):
+        pass
+
+
+class DataBackend():
+    """ Holds BLOBs, never overwrites
+    """
+
+    def __init__(self, path):
+        self.path = path
+
+
+    def save(self, data):
+        """ Saves data, returns unique ID """
+        raise NotImplementedError()
+
+
+    def read(self, uid):
+        """ Returns b'<data>' or raises FileNotFoundError """
+        raise NotImplementedError()
+
+
+    def rm(self, uid):
+        """ Deletes a block """
+        raise NotImplementedError()
+
+
+    def close(self):
+        pass
+
+
+class SQLiteBackend(MetaBackend):
+    """ Stores meta data in a sqlite database """
+
+    DBFILENAME = 'backy.sqlite'
+
+    def __init__(self, path):
+        MetaBackend.__init__(self, path)
+        dbpath = os.path.join(self.path, self.DBFILENAME)
+
+        def dict_factory(cursor, row):
+            """ A row factory for sqlite3 which emulates a dict cursor. """
+            d = {}
+            for idx, col in enumerate(cursor.description):
+                d[col[0]] = row[idx]
+            return d
+
+        self.conn = sqlite3.connect(dbpath)
+        self.conn.row_factory = dict_factory
+        self.cursor = self.conn.cursor()
+        self._create()
+
+
+    def _uid(self):
+        return str(uuid.uuid1())
+
+
+    def _now(self):
+        """ Returns datetime as isoformat (ex. 2015-10-25T10:43:03.823777+00:00) """
+        return datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
+
+
+    def _create(self):
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS versions
+             (uid text, date text, name text, size integer)''')
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS blocks
+             (uid text, version_uid text, id integer, date text, checksum text, size integer)''')
+        self.conn.commit()
+
+
+    def create_version(self, version_name, size):
+        uid = self._uid()
+        now = self._now()
+        self.cursor.execute('''
+            INSERT INTO versions (uid, date, name, size) VALUES (?, ?, ?, ?)
+            ''', (uid, now, version_name, size))
+        self.conn.commit()
+        return uid
+
+
+    def get_version(self, uid):
+        self.cursor.execute('''
+            SELECT uid, date, name, size FROM versions WHERE uid=?
+            ''', (uid,))
+        version = self.cursor.fetchone()  # might raise
+        if version is None:
+            # not found
+            raise KeyError('Version {} not found.'.format(uid))
+        return version
+
+
+
+    def set_block(self, id, version_uid, block_uid, checksum, size):
+        now = self._now()
+        self.cursor.execute('''
+            INSERT INTO blocks (uid, version_uid, id, date, checksum, size) VALUES (?, ?, ?, ?, ?, ?)
+            ''', (block_uid, version_uid, id, now, checksum, size))
+        self.conn.commit()
+
+
+    def close(self):
+        self.conn.close()
+
+
+class FileBackend(DataBackend):
+    """ A DataBackend which stores in files. The files are stored in directories
+    starting with the bytes of the generated uid. The depth of this structure
+    is configurable via the DEPTH parameter, which defaults to 2. """
+
+    DEPTH = 2
+    SPLIT = 2
+
+    def _uid(self):
+        return uuid.uuid1().hex
+
+
+    def _path(self, uid):
+        """ Returns a generated path (depth = self.DEPTH) from a uid.
+        Example uid=831bde887afc11e5b45aa44e314f9270 and depth=2, then
+        it returns "83/1b".
+        If depth is larger than available bytes, then available bytes
+        are returned only as path."""
+
+        parts = [uid[i:i+self.SPLIT] for i in range(0, len(uid), self.SPLIT)]
+        return os.path.join(*parts[:self.DEPTH])
+
+
+    def save(self, data):
+        uid = self._uid()
+        path = os.path.join(self.path, self._path(uid))
+        makedirs(path)
+        filename = os.path.join(path, uid)
+        if os.path.exists(filename):
+            raise ValueError('Found a file {} where this is impossible.'.format(filename))
+        with open(filename, 'wb') as f:
+            r = f.write(data)
+            assert r == len(data)
+        return uid
+
+
+    def rm(self, uid):
+        path = os.path.join(self.path, self._path(uid))
+        filename = os.path.join(path, uid)
+        if not os.path.exists(filename):
+            raise FileNotFoundError('File {} not found.'.format(filename))
+        os.unlink(filename)
+
+
+    def read(self, uid):
+        path = os.path.join(self.path, self._path(uid))
+        filename = os.path.join(path, uid)
+        if not os.path.exists(filename):
+            raise FileNotFoundError('File {} not found.'.format(filename))
+        return open(filename, 'rb').read()
+
+
+
+class Backy():
+    """
+    """
+
+    def __init__(self, path):
+        self.path = path
+        self.datapath = os.path.join(self.path, 'data')
+        self.meta_backend = SQLiteBackend(self.datapath)
+        self.data_backend = FileBackend(self.datapath)
+        makedirs(self.datapath)
+
+
+    def close(self):
+        self.meta_backend.close()
+        self.data_backend.close()
+
+
+
 class Commands():
     """Proxy between CLI calls and actual backup code."""
 
@@ -54,17 +270,13 @@ class Commands():
 
 
     def backup(self, source, backupname, rbd):
+        backy = Backy(self.path)
         hints = None
         if rbd:
             data = ''.join([line for line in fileinput.input(rbd).readline()])
             hints = hints_from_rbd_diff(data)
-            print(hints)
-            # TODO: next
-            # if '-' get from stdin
-            # else read from file
-            # convert rbd into hints
+        import pdb; pdb.set_trace()
 
-        backy = Backy(self.path, backupname, chunk_size=CHUNK_SIZE)
         backy.backup(source, hints)
 
 
--- src/backy2/tests/test_main.py
+++ src/backy2/tests/test_main.py
@@ -1,7 +1,7 @@
 import pytest
 import os
 import sys
-import backy2.main
+import backy2.backy
 import shutil
 import time
 import random
@@ -28,374 +28,64 @@ def test_path(request):
     return path
 
 
-def test_display_usage(capsys, argv):
-    with pytest.raises(SystemExit) as exit:
-        backy2.main.main()
-    assert exit.value.code == 0
-    out, err = capsys.readouterr()
-    assert """\
-usage: py.test [-h] [-v] [-b BACKUPDIR] {backup,restore,scrub,cleanup,ls} ...
-""" == out
-    assert err == ""
+def test_FileBackend_path(test_path):
+    uid = 'c2cac25a7afd11e5b45aa44e314f9270'
+
+    backend = backy2.backy.FileBackend(test_path)
+    backend.DEPTH = 2
+    backend.SPLIT = 2
+    path = backend._path(uid)
+    assert path == 'c2/ca'
+
+    backend.DEPTH = 3
+    backend.SPLIT = 2
+    path = backend._path(uid)
+    assert path == 'c2/ca/c2'
+
+    backend.DEPTH = 3
+    backend.SPLIT = 3
+    path = backend._path(uid)
+    assert path == 'c2c/ac2/5a7'
+
+    backend.DEPTH = 3
+    backend.SPLIT = 1
+    path = backend._path(uid)
+    assert path == 'c/2/c'
+
+    backend.DEPTH = 1
+    backend.SPLIT = 2
+    path = backend._path(uid)
+    assert path == 'c2'
+
+    backend.close()
+
+
+def test_FileBackend_save_read(test_path):
+    backend = backy2.backy.FileBackend(test_path)
+    uid = backend.save(b'test')
+    assert backend.read(uid) == b'test'
+    backend.close()
+
+
+def test_SQLiteBackend_create_version(test_path):
+    backend = backy2.backy.SQLiteBackend(test_path)
+    name = 'backup-mysystem1-20150110140015'
+    uid = backend.create_version(name, 10)
+    assert(uid)
+    version = backend.get_version(uid)
+    assert version['name'] == name
+    assert version['size'] == 10
+    assert version['uid'] == uid
+    backend.close()
+
+
+def test_SQLiteBackend_version_not_found(test_path):
+    backend = backy2.backy.SQLiteBackend(test_path)
+    with pytest.raises(KeyError) as e:
+        backend.get_version('123')
+    assert str(e.exconly()) == "KeyError: 'Version 123 not found.'"
+    backend.close()
 
-
-
-# Test Level
-
-def test_level_consistency(test_path):
-    s1 = os.urandom(CHUNK_SIZE_MIN)
-    s2 = os.urandom(CHUNK_SIZE_MIN)
-    s3 = os.urandom(CHUNK_SIZE_MIN)
-    data_file = os.path.join(test_path, '_test.data')
-    index_file = os.path.join(test_path, '_test.index')
-    with backy2.main.Level(data_file, index_file, CHUNK_SIZE_MIN) as lw:
-        lw.write(10, s1)
-        lw.write(8, s2)
-        lw.write(12, s3)
-
-    with backy2.main.Level(data_file, index_file, CHUNK_SIZE_MIN) as lw:
-        assert lw.read(8) == s2
-        assert lw.read(10) == s1
-        assert lw.read(12) == s3
-
-
-def test_level_wrong_size(test_path):
-    s1 = os.urandom(10)
-    s2 = os.urandom(10)
-    s3 = os.urandom(CHUNK_SIZE_MIN + 1)
-    data_file = os.path.join(test_path, '_test.data')
-    index_file = os.path.join(test_path, '_test.index')
-    with backy2.main.Level(data_file, index_file, CHUNK_SIZE_MIN) as lw:
-        lw.write(1, s1)
-        lw.write(2, s2)
-        with pytest.raises(backy2.main.BackyException):
-            lw.write(1, s3)
-
-
-def test_level_wrong_size_last_chunk(test_path):
-    s1 = os.urandom(10)
-    s2 = os.urandom(10)
-    s3 = os.urandom(11)
-    data_file = os.path.join(test_path, '_test.data')
-    index_file = os.path.join(test_path, '_test.index')
-    with backy2.main.Level(data_file, index_file, CHUNK_SIZE) as lw:
-        lw.write(1, s1)
-        lw.write(2, s2)
-        lw.write(2, s3)
-
-    with backy2.main.Level(data_file, index_file, CHUNK_SIZE) as lw:
-        assert lw.read(1) == s1
-        assert lw.read(2) == s3
-
-
-def test_level_wrong_checksum(caplog, test_path):
-    s1 = os.urandom(10)
-    data_file = os.path.join(test_path, '_test.data')
-    index_file = os.path.join(test_path, '_test.index')
-    with backy2.main.Level(data_file, index_file, CHUNK_SIZE) as lw:
-        lw.write(1, s1)
-        # nasty hack, destroy checksum for chunk_id 1
-        lw.index.get(1).checksum = 'haha'
-
-    with backy2.main.Level(data_file, index_file, CHUNK_SIZE) as lw:
-        lw.read(1)
-        assert 'CRITICAL Checksum for chunk 1 does not match' in caplog.text()
-
-
-
-# Test Backup
-
-def test_backup(test_path):
-    backy = backy2.main.Backy(test_path, 'backup', CHUNK_SIZE_MIN)
-
-    data_1 = os.urandom(4*CHUNK_SIZE_MIN)                           # 4 complete chunks
-    data_2 = data_1 + os.urandom(10)                                # append 10 bytes
-    data_3 = os.urandom(CHUNK_SIZE_MIN) + data_2[CHUNK_SIZE_MIN:]   # Change 1st chunk
-    data_4 = data_3 + os.urandom(10)                                # append 10 bytes
-
-    src_1 = os.path.join(test_path, 'data_1')
-    src_2 = os.path.join(test_path, 'data_2')
-    src_3 = os.path.join(test_path, 'data_3')
-    src_4 = os.path.join(test_path, 'data_4')
-
-    # this test backups and restores the generated data files and
-    # tests them after restoring against filesize and content.
-
-    # create backups
-
-    with open(src_1, 'wb') as f:
-        f.write(data_1)
-    with open(src_2, 'wb') as f:
-        f.write(data_2)
-    with open(src_3, 'wb') as f:
-        f.write(data_3)
-    with open(src_4, 'wb') as f:
-        f.write(data_4)
-
-    restore = os.path.join(test_path, 'restore')
-
-    # 1st day, test backup
-    backy.backup(src_1)
-    # restore of level 0 is a 0 byte file.
-    backy.restore(restore)
-    assert open(restore, 'rb').read() == data_1
-    backy.restore(restore, 0)
-    assert open(restore, 'rb').read() == b''
-
-    # 2nd day, test both backups
-    backy.backup(src_2)
-    backy.restore(restore)
-    assert open(restore, 'rb').read() == data_2
-    backy.restore(restore, 1)
-    assert open(restore, 'rb').read() == data_1
-    backy.restore(restore, 0)
-    assert open(restore, 'rb').read() == b''
-
-    # 3rd day, test all backups
-    backy.backup(src_3)
-    backy.restore(restore)
-    assert open(restore, 'rb').read() == data_3
-    backy.restore(restore, 2)
-    assert open(restore, 'rb').read() == data_2
-    backy.restore(restore, 1)
-    assert open(restore, 'rb').read() == data_1
-    backy.restore(restore, 0)
-    assert open(restore, 'rb').read() == b''
-
-    # 4th day, test all backups
-    backy.backup(src_4)
-    backy.restore(restore)
-    assert open(restore, 'rb').read() == data_4
-    backy.restore(restore, 3)
-    assert open(restore, 'rb').read() == data_3
-    backy.restore(restore, 2)
-    assert open(restore, 'rb').read() == data_2
-    backy.restore(restore, 1)
-    assert open(restore, 'rb').read() == data_1
-    backy.restore(restore, 0)
-    assert open(restore, 'rb').read() == b''
-
-
-def test_restore_wrong_checksum(test_path, caplog):
-    backy = backy2.main.Backy(test_path, 'backup', CHUNK_SIZE_MIN)
-
-    data = os.urandom(4*CHUNK_SIZE_MIN)   # 4 complete chunks
-    src = os.path.join(test_path, 'data')
-    with open(src, 'wb') as f:
-        f.write(data)
-
-    backy.backup(src)
-
-    # test if all is good
-    backy.restore(os.path.join(test_path, 'restore'))
-    assert 'CRITICAL Checksum for chunk 0 does not match' not in caplog.text()
-
-    # change something (i.e. sun flare changes some bits)
-    backup_data = open(backy.data_filename(), 'rb').read()
-    _ = list(backup_data)
-    _[0] = (_[0] + 1) % 256
-    backup_data = bytes(_)
-    with open(backy.data_filename(), 'wb') as f:
-        f.write(backup_data)
-
-    # test if all is good
-    backy.restore(os.path.join(test_path, 'restore'))
-    assert 'CRITICAL Checksum for chunk 0 does not match' in caplog.text()
-
-
-def test_scrub_wrong_checksum(test_path, caplog):
-    backy = backy2.main.Backy(test_path, 'backup', CHUNK_SIZE_MIN)
-
-    data = os.urandom(4*CHUNK_SIZE_MIN)   # 4 complete chunks
-    src = os.path.join(test_path, 'data')
-    with open(src, 'wb') as f:
-        f.write(data)
-
-    backy.backup(src)
-
-    # test if all is good
-    backy.scrub()
-    assert 'SCRUB: Checksum for chunk' not in caplog.text()
-    assert backy2.main.Level(backy.data_filename(), backy.index_filename(), CHUNK_SIZE_MIN).open().read_meta(0).status == backy2.main.CHUNK_STATUS_EXISTS
-
-    # change something in backup data (i.e. sun flare changes some bits)
-    backup_data = open(backy.data_filename(), 'rb').read()
-    _ = list(backup_data)
-    _[0] = (_[0] + 1) % 256  # add 1 to the first byte
-    backup_data = bytes(_)
-    with open(backy.data_filename(), 'wb') as f:
-        f.write(backup_data)
-
-    # test if all is good
-    backy.scrub()
-    assert 'SCRUB: Checksum for chunk 0 does not match.' in caplog.text()
-    assert backy2.main.Level(backy.data_filename(), backy.index_filename(), CHUNK_SIZE_MIN).open().read_meta(0).status == backy2.main.CHUNK_STATUS_DESTROYED
-
-
-def test_deep_scrub_wrong_data(test_path, caplog):
-    backy = backy2.main.Backy(test_path, 'backup', CHUNK_SIZE_MIN)
-
-    data = os.urandom(4*CHUNK_SIZE_MIN)   # 4 complete chunks
-    src = os.path.join(test_path, 'data')
-    with open(src, 'wb') as f:
-        f.write(data)
-
-    backy.backup(src)
-
-    # test if all is good
-    backy.deep_scrub(src)
-    assert 'SCRUB: Source data for chunk' not in caplog.text()
-    assert backy2.main.Level(backy.data_filename(), backy.index_filename(), CHUNK_SIZE_MIN).open().read_meta(0).status == backy2.main.CHUNK_STATUS_EXISTS
-
-    # change something in source data (i.e. sun flare changes some bits)
-    src_data = open(src, 'rb').read()
-    _ = list(src_data)
-    _[0] = (_[0] + 1) % 256  # add 1 to the first byte
-    src_data = bytes(_)
-    with open(src, 'wb') as f:
-        f.write(src_data)
-
-    # test if all is good
-    backy.deep_scrub(src)
-    assert 'SCRUB: Source data for chunk 0 does not match.' in caplog.text()
-    assert backy2.main.Level(backy.data_filename(), backy.index_filename(), CHUNK_SIZE_MIN).open().read_meta(0).status == backy2.main.CHUNK_STATUS_DESTROYED
-
-
-def test_deep_scrub_performance_percentile(test_path):
-    """ This test could behave better. When system i/o is heavy, this could fail
-    because we're measuring performance here..."""
-    backy = backy2.main.Backy(test_path, 'backup', CHUNK_SIZE)
-
-    src = os.path.join(test_path, 'data')
-    with open(src, 'wb') as f:
-        f.write(b'\0' * CHUNK_SIZE * 10)
-
-    backy.backup(src)
-
-    t = time.time()
-    c1 = backy.deep_scrub(src)
-    dt1 = time.time() - t
-
-    t = time.time()
-    c2 = backy.deep_scrub(src, percentile=50)
-    dt2 = time.time() - t
-
-    assert dt1 > dt2
-    assert c1 > c2
-
-
-def test_hints(test_path):
-    def write_chunk(f, chunk_id, offset, length):
-        f.seek(CHUNK_SIZE_MIN*chunk_id + offset)
-        _from = f.tell()
-        _length = f.write(os.urandom(length))
-        return _from, _length
-
-    backy = backy2.main.Backy(test_path, 'backup', CHUNK_SIZE_MIN)
-
-    src = os.path.join(test_path, 'data')
-    #open(src, 'wb').write(os.urandom(CHUNK_SIZE_MIN*8))
-    open(src, 'wb').write(b'\0' * CHUNK_SIZE_MIN*8)
-    backy.backup(src)
-
-    # change a chunk and backup using hints
-    with open(src, 'r+b') as f:
-        _from, length = write_chunk(f, 3, 10, 30)
-    backy.backup(src, hints=[(_from, length, True)])
-
-    assert open(src, 'rb').read() == open(backy.data_filename(), 'rb').read()
-
-
-def test_chunks_from_hints():
-    hints = [
-        (10, 100, True),
-        (1024, 2048, True),
-        (4096, 3000, True),
-        (14000, 10, True),
-        (16383, 1025, True),
-        ]
-    #         0          1, 2          4, 5, 6       13,          15, 16
-    chunk_size = 1024
-    cfh = backy2.main.chunks_from_hints(hints, chunk_size)
-    assert sorted(list(cfh)) == [0, 1, 2, 4, 5, 6, 13, 15, 16]
-
-
-# test Index
-
-def test_index(test_path):
-    index = backy2.main.Index(CHUNK_SIZE_MIN)
-    index_filename = os.path.join(test_path, 'backup..index')
-    with pytest.raises(FileNotFoundError):
-        index.read(index_filename)
-
-    # new chunks
-
-    chunk12 = index.get(12)
-    assert chunk12.offset == 0 * CHUNK_SIZE_MIN
-    chunk5 = index.get(5)
-    assert chunk5.offset == 1 * CHUNK_SIZE_MIN
-    chunk8 = index.get(8)
-    assert chunk8.offset == 2 * CHUNK_SIZE_MIN
-
-    # see if they stay where they are
-
-    chunk12 = index.get(12)
-    assert chunk12.offset == 0 * CHUNK_SIZE_MIN
-    chunk5 = index.get(5)
-    assert chunk5.offset == 1 * CHUNK_SIZE_MIN
-    chunk8 = index.get(8)
-    assert chunk8.offset == 2 * CHUNK_SIZE_MIN
-
-    # write
-
-    index.write(index_filename)
-
-    # read
-
-    index = backy2.main.Index(CHUNK_SIZE_MIN)
-    index.read(index_filename)
-
-    # see if they still are where they should be
-
-    chunk8 = index.get(8)
-    assert chunk8.offset == 2 * CHUNK_SIZE_MIN
-    chunk5 = index.get(5)
-    assert chunk5.offset == 1 * CHUNK_SIZE_MIN
-    chunk12 = index.get(12)
-    assert chunk12.offset == 0 * CHUNK_SIZE_MIN
-
-    # add one
-
-    chunk1 = index.get(1)
-    assert chunk1.offset == 3 * CHUNK_SIZE_MIN
-
-    # change one
-    #
-    chunk12.checksum = '123'
-
-    # write
-
-    index.write(index_filename)
-
-    # read
-
-    index = backy2.main.Index(CHUNK_SIZE_MIN)
-    index.read(index_filename)
-
-    # check again
-
-    chunk1 = index.get(1)
-    assert chunk1.offset == 3 * CHUNK_SIZE_MIN
-    chunk8 = index.get(8)
-    assert chunk8.offset == 2 * CHUNK_SIZE_MIN
-    chunk5 = index.get(5)
-    assert chunk5.offset == 1 * CHUNK_SIZE_MIN
-    chunk12 = index.get(12)
-    assert chunk12.offset == 0 * CHUNK_SIZE_MIN
-    assert chunk12.checksum == '123'
-
-# TODO: test remove() method
-# TODO: test initial sparse base
 
 def _patch(filename, offset, data=None):
     """ write data into a file at offset """
@@ -405,115 +95,3 @@ def _patch(filename, offset, data=None):
         f.seek(offset)
         f.write(data)
 
-def test_initial_sparse_backup(test_path):
-    backy = backy2.main.Backy(test_path, 'backup', CHUNK_SIZE_MIN)
-
-    src = os.path.join(test_path, 'data')
-    hints = []
-    for i in range(32):
-        if random.randint(0, 1):  # 50/50
-            _patch(src, i * CHUNK_SIZE_MIN, os.urandom(CHUNK_SIZE_MIN))
-            hints.append((i * CHUNK_SIZE_MIN, CHUNK_SIZE_MIN, True))
-        else:
-            _patch(src, i * CHUNK_SIZE_MIN, b'\0' * CHUNK_SIZE_MIN)
-
-    backy.backup(src, hints)
-
-    restore = os.path.join(test_path, 'restore')
-    backy.restore(restore)
-    checked = backy.deep_scrub(src)  # this must not raise or else restore is defect
-    assert checked == len(hints)
-
-
-def test_initial_sparse_backup_with_sparse_beginning_and_end(test_path):
-    backy = backy2.main.Backy(test_path, 'backup', CHUNK_SIZE_MIN)
-
-    src = os.path.join(test_path, 'data')
-    hints = []
-    for i in range(32):
-        if random.randint(0, 1) or i in (0, 31):
-            _patch(src, i * CHUNK_SIZE_MIN, os.urandom(CHUNK_SIZE_MIN))
-            hints.append((i * CHUNK_SIZE_MIN, CHUNK_SIZE_MIN, True))
-        else:
-            _patch(src, i * CHUNK_SIZE_MIN, b'\0' * CHUNK_SIZE_MIN)
-
-    backy.backup(src, hints)
-
-    restore = os.path.join(test_path, 'restore')
-    backy.restore(restore)
-    checked = backy.deep_scrub(src)  # this must not raise or else restore is defect
-    assert checked == len(hints)
-
-
-def test_initial_sparse_with_levels(test_path):
-    backy = backy2.main.Backy(test_path, 'backup', CHUNK_SIZE_MIN)
-
-    src = os.path.join(test_path, 'data')
-    hints = []
-    in_base = set()  # chunk_ids
-    for i in range(32):
-        if random.randint(0, 1) or i in (0, 31):
-            _patch(src, i * CHUNK_SIZE_MIN, os.urandom(CHUNK_SIZE_MIN))
-            hints.append((i * CHUNK_SIZE_MIN, CHUNK_SIZE_MIN, True))
-            in_base.add(i)
-        else:
-            _patch(src, i * CHUNK_SIZE_MIN, b'\0' * CHUNK_SIZE_MIN)
-
-    backy.backup(src, hints)
-    restore = os.path.join(test_path, 'restore')
-    backy.restore(restore)
-    checked = backy.deep_scrub(src)  # this must not raise or else restore is defect
-    assert checked == len(in_base)
-
-    # change blocks
-    hints = []
-    for i in range(32):
-        if random.randint(0, 1):
-            _patch(src, i * CHUNK_SIZE_MIN, os.urandom(CHUNK_SIZE_MIN))
-            hints.append((i * CHUNK_SIZE_MIN, CHUNK_SIZE_MIN, True))
-            in_base.add(i)
-
-    backy.backup(src, hints)
-    restore = os.path.join(test_path, 'restore')
-    backy.restore(restore)
-    checked = backy.deep_scrub(src)  # this must not raise or else restore is defect
-    assert checked == len(in_base)
-
-
-def test_multiple_sparse_with_levels(test_path):
-    backy = backy2.main.Backy(test_path, 'backup', CHUNK_SIZE_MIN)
-
-    src = os.path.join(test_path, 'data')
-    hints = []
-    in_base = dict()  # chunk_ids
-    for i in range(32):
-        _patch(src, i * CHUNK_SIZE_MIN, os.urandom(CHUNK_SIZE_MIN))
-        exists = bool(random.randint(0, 1))
-        hints.append((i * CHUNK_SIZE_MIN, CHUNK_SIZE_MIN, exists))
-        in_base[i] = exists
-
-    backy.backup(src, hints)
-    restore = os.path.join(test_path, 'restore')
-    backy.restore(restore)
-    checked = backy.deep_scrub(src)  # this must not raise or else restore is defect
-    should_be = [x for x in in_base.items() if x[1]]
-    assert checked == len(should_be)
-
-    # change blocks
-    hints = []
-    for i in range(32):
-        _patch(src, i * CHUNK_SIZE_MIN, os.urandom(CHUNK_SIZE_MIN))
-        exists = bool(random.randint(0, 1))
-        hints.append((i * CHUNK_SIZE_MIN, CHUNK_SIZE_MIN, exists))
-        in_base[i] = exists
-
-    backy.backup(src, hints)
-    restore = os.path.join(test_path, 'restore')
-    backy.restore(restore)
-    checked = backy.deep_scrub(src)  # this must not raise or else restore is defect
-    should_be = [x for x in in_base.items() if x[1]]
-    assert checked == len(should_be)
-
-
-# TODO: test cleanup
-# TODO: test ls
