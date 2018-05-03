efff565997f8b78d56fc8937ec22cb606cc2d7d9
Lars Fenneberg <lf@elemental.net>
2018-05-03 14:30:53 +0200

    Readd rmtree

--- src/backy2/tests/testcase.py
+++ src/backy2/tests/testcase.py
@@ -5,6 +5,7 @@ from binascii import hexlify
 import importlib
 import os
 import random
+import shutil
 
 from backy2.config import Config
 from backy2.data_backends import DataBackend
@@ -34,7 +35,7 @@ class TestCase():
 
         def close(self):
             pass
-            #shutil.rmtree(self.path)
+            shutil.rmtree(self.path)
 
     def setUp(self):
         self.testpath = self.TestPath()
