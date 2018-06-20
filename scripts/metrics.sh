2701f5f7d5b75eedb54a2561ca2a37fdf9eb2fa7
Lars Fenneberg <lf@elemental.net>
2018-06-21 00:00:47 +0200

    Add new standalone Docker image

--- scripts/metrics.sh
+++ scripts/metrics.sh
@@ -1,5 +1,4 @@
-# Version related
 io::prometheus::NewGauge name=benji_job_start_time labels=action,type,version_name help='Start time of job (time_t)'
 io::prometheus::NewGauge name=benji_job_completion_time labels=action,type,version_name help='Completion time of job (time_t)'
-#io::prometheus::NewGauge name=benji_job_status_succeeded labels=action,type,version_name help='Job succeeded'
-#io::prometheus::NewGauge name=benji_job_status_failed labels=action,type,version_name help='Job failed'
+io::prometheus::NewGauge name=benji_job_status_succeeded labels=action,type,version_name help='Job succeeded'
+io::prometheus::NewGauge name=benji_job_status_failed labels=action,type,version_name help='Job failed'
