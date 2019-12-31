5c81d6fbdc23ed6fcfdf2b085724a78dd516c579
Daniel Kraft <daniel.kraft@d9t.de>
2019-12-31 12:10:57 +0100

    For the sake of the demo don't use most performant but most understandable parameters.

--- demo/cleanup.sh
+++ demo/cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for version in `backy2 -ms ls -e -f uid`; do
-    backy2 rm $version
+    backy2 rm -f $version
 done
-backy2 cleanup  # note: space is freed after 24h.
+backy2 cleanup -f  # note: space is freed after 24h.
