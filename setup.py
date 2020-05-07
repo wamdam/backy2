b445a0eb84bb9c99af04297833532c6b05da5a4b
Daniel Kraft <daniel.kraft@d9t.de>
2020-05-07 15:08:05 +0200

    fusepy is a requirement

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ Topic :: System :: Archiving :: Backup
         'setproctitle>=1.1.8',
         'python-dateutil>=2.6.0',
         'alembic>=0.7.5',
+        'fusepy',
         #'pycryptodome>=3.6.1,<4',
         #'zstandard>=0.9.0',
         #'boto>=2.38.0',
