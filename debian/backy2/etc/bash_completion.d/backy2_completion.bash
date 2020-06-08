40ccfae4d5d1d283d6896bb90161708134443a77
Daniel Kraft <daniel.kraft@d9t.de>
2020-06-08 10:28:09 +0200

    Bash completion

--- debian/backy2/etc/bash_completion.d/backy2_completion.bash
+++ debian/backy2/etc/bash_completion.d/backy2_completion.bash
@@ -8,11 +8,41 @@ _backy2_names() {
     suggestions=($(compgen -W "$(backy2 -ms ls -f name|sort|uniq)" -- "${COMP_WORDS[2]}"))
 }
 
+_backy2_commands() {
+    suggestions=($(compgen -W "$(backy2|grep -oP -e "{.*}"|sed 's/[{}]//g'|sed 's/,/ /g')" -- "${COMP_WORDS[1]}"))
+}
+
 _backy2_completions() {
     if [[ "${COMP_WORDS[1]}" == "ls" ]]; then
         _backy2_names
     elif [[ "${COMP_WORDS[1]}" == "restore" ]]; then
         _backy2_versions
+    elif [[ "${COMP_WORDS[1]}" == "protect" ]]; then
+        _backy2_versions
+    elif [[ "${COMP_WORDS[1]}" == "unprotect" ]]; then
+        _backy2_versions
+    elif [[ "${COMP_WORDS[1]}" == "rm" ]]; then
+        _backy2_versions
+    elif [[ "${COMP_WORDS[1]}" == "scrub" ]]; then
+        _backy2_versions
+    elif [[ "${COMP_WORDS[1]}" == "export" ]]; then
+        _backy2_versions
+    elif [[ "${COMP_WORDS[1]}" == "du" ]]; then
+        _backy2_versions
+    elif [[ "${COMP_WORDS[1]}" == "migrate-encryption" ]]; then
+        _backy2_versions
+    elif [[ "${COMP_WORDS[1]}" == "add-tag" ]]; then
+        _backy2_versions
+    elif [[ "${COMP_WORDS[1]}" == "remove-tag" ]]; then
+        _backy2_versions
+    elif [[ "${COMP_WORDS[1]}" == "expire" ]]; then
+        _backy2_versions
+    elif [[ "${COMP_WORDS[1]}" == "due" ]]; then
+        _backy2_names
+    elif [[ "${COMP_WORDS[1]}" == "sla" ]]; then
+        _backy2_names
+    else
+        _backy2_commands
     fi
 
     #local suggestions=($(compgen -W "$(backy2 -ms ls -f uid)" -- "${COMP_WORDS[2]}"))
