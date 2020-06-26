fc04c13fa32aa03e6e8424d054c18243ac394e9f
Daniel Kraft <daniel.kraft@d9t.de>
2020-06-26 11:19:06 +0200

    Added debug logging

--- src/backy2/logging.py
+++ src/backy2/logging.py
@@ -6,9 +6,9 @@ import sys
 
 logger = logging.getLogger(__name__)
 
-def init_logging(logfile, console_level):  # pragma: no cover
+def init_logging(logfile, console_level, debug=False):  # pragma: no cover
     console = logging.StreamHandler(sys.stdout)
-    console.setFormatter(logging.Formatter('%(levelname)8s: %(message)s')),
+    console.setFormatter(logging.Formatter('%(levelname)8s: [%(name)s] %(message)s')),
     console.setLevel(console_level)
     #logger.addHandler(console)
 
@@ -19,11 +19,25 @@ def init_logging(logfile, console_level):  # pragma: no cover
 
     logging.basicConfig(handlers = [console, logfile], level=logging.DEBUG)
 
-    # make alembic quiet
     logging.getLogger('alembic').setLevel(logging.WARN)
     logging.getLogger('boto3').setLevel(logging.WARN)
     logging.getLogger('botocore').setLevel(logging.WARN)
     logging.getLogger('urllib3').setLevel(logging.WARN)
+
+    if debug:  # i.e. "-d" switch
+        # debugging
+        #loggers = [logging.getLogger(name) for name in logging.root.manager.loggerDict]
+        console.setLevel(logging.DEBUG)
+        logging.basicConfig(level=logging.DEBUG)
+        logging.getLogger('backy2.logging').setLevel(logging.DEBUG)
+        logging.getLogger('backy2').setLevel(logging.DEBUG)
+        logging.getLogger('alembic').setLevel(logging.DEBUG)
+        logging.getLogger('boto3').setLevel(logging.DEBUG)
+        logging.getLogger('botocore').setLevel(logging.DEBUG)
+        logging.getLogger('urllib3').setLevel(logging.DEBUG)
+        log = logging.getLogger('requests.packages.urllib3')
+        log.setLevel(logging.DEBUG)
+        log.propagate = True
 
     logger.info('$ ' + ' '.join(sys.argv))
 
