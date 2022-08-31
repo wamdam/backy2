4bb257761b79426cbf0a1c23e09e2fc75b72a57b
David Caro <dcaro@wikimedia.org>
2022-08-31 10:31:27 +0200

    Add support for sqlalchemy >=1.4
    Main changes there are that subqueries have to be cast to select, and
    delete needs a table, not a column.

--- src/backy2/meta_backends/sql.py
+++ src/backy2/meta_backends/sql.py
@@ -555,7 +555,13 @@ class MetaBackend(_MetaBackend):
     def cleanup_delete_candidates(self, dt=3600):
         # Delete false positives:
         logger.info("Deleting false positives...")
-        self.session.query(DeletedBlock.uid).filter(DeletedBlock.uid.in_(self.session.query(Block.uid).distinct(Block.uid).filter(Block.uid.isnot(None)).subquery())).filter(DeletedBlock.time < (inttime() - dt)).delete(synchronize_session=False)
+        self.session.query(
+            DeletedBlock
+        ).filter(
+            DeletedBlock.uid.in_(
+                self.session.query(Block.uid).distinct(Block.uid).filter(Block.uid.isnot(None)).subquery().select()
+            )
+        ).filter(DeletedBlock.time < (inttime() - dt)).delete(synchronize_session=False)
         logger.info("Deleting false positives: done. Now deleting blocks.")
         self.session.commit()
 
